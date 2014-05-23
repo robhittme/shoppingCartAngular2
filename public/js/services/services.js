a964ed4fcdcef00f2f9176f159636955e99fa95d
Robert Hitt <rphitt1@gmail.com>
2014-05-23 10:57:15 -0400

    transfering the addService into services.js

--- public/js/services/services.js
+++ public/js/services/services.js
@@ -20,3 +20,31 @@ angular.module('ngDay2App')
 			}
 			)
 	});
+
+angular.module('shoppingCart20App')
+	.factory('AddService', function($resource){
+		return $resource('http://tiy-fee-rest.herokuapp.com/collections/robhittmeCart', {},
+			{
+				getAllProducts: {
+					method: 'GET',
+					isArray: true
+				},
+				createNewProduct: {
+					method: 'POST'
+				}
+			});
+	})
+
+	.factory('PostService', function($resource) {
+		return $resource('http://tiy-fee-rest.herokuapp.com/collections/robhittmeCart/:id', 
+		{
+			id: '@_id'
+		}, 
+		{
+			showProduct: { method: 'GET'	},
+			updateProduct: { method: 'PUT'	},
+			deleteProduct: { method: 'DELETE'	}
+
+
+		});
+	});
