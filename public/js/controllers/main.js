3d4cbcc21fbd22fd2c0674e7a811add6a32d4894
Robert Hitt <rphitt1@gmail.com>
2014-05-26 15:25:34 -0400

    syntax error fix

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -41,34 +41,22 @@ angular.module('ngDay2App')
       };
 
   }])
-  .controller('PostCtrl', function($scope, $location, $routeParams, PostSvc) {
-
-    $scope.post = PostSvc.show({ id: $routeParams.id });
-    $scope.delete = function() { 
-      PostSvc.delete({ id: $routeParams.id });
-      $location.path('/blog');
-    };
-    $scope.edit = function() {
-      PostSvc.edit($scope.post);
-      $location.path('/blog');
-    };
-
-  })
+  
   .controller('UDCtrl', function($scope, $location, $routeParams, PostService) {
     
     $scope.product = PostService.showProduct({ id: $routeParams.id });
-
-    $scope.updateProduct = function() {
-      PostService.updateProduct($scope.product);
-      $location.path('/Home/' + $routeParams.id);
-    };
-
-    $scope.delete = function() 
+    $scope.delete = function(); 
     console.log("its working");
     {
       PostService.deleteProduct({ id: $routeParams.id });
       // $location.path('/Home');
     };
+    $scope.updateProduct = function() {
+      PostService.updateProduct($scope.product);
+      $location.path('/Home/' + $routeParams.id);
+    };
+
+    
     // editing
     // deleting
     // showing
